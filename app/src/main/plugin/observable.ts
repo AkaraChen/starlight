4c7a6f6a6cf44431a9dff08c14fc962a4732da6b
AkaraChen <akarachen@outlook.com>
2024-03-24 00:36:49 +0800

    chore: upd

--- app/src/main/plugin/observable.ts
+++ app/src/main/plugin/observable.ts
@@ -1,5 +1,5 @@
-import { IPlugin } from '@starlight-app/plugin-sdk'
-import { BehaviorSubject, map } from 'rxjs'
+import { ICommand, IPlugin } from '@starlight-app/plugin-sdk'
+import { BehaviorSubject, Observable, map } from 'rxjs'
 
 export const pluginSubject = new BehaviorSubject<IPlugin[]>([])
 
@@ -10,3 +10,27 @@ export const staticCommands$ = pluginSubject.pipe(
 export const views$ = pluginSubject.pipe(
   map((plugins) => plugins.flatMap((plugin) => plugin.views ?? []))
 )
+
+// TODO: add open searchable view commands here
+
+export const searchCommands$ = (query: string): Observable<ICommand[]> => {
+  const abortController = new AbortController()
+  setTimeout(() => {
+    abortController.abort('Search command timeout')
+  }, 1000)
+  return new Observable<ICommand[]>((subscriber) => {
+    // TODO: merge with searchable view commands
+    pluginSubject.value.forEach(async (plugin) => {
+      try {
+        const result = await plugin.search?.(query, abortController.signal)
+        if (result) {
+          subscriber.next(result)
+        }
+      } catch (error) {
+        if (error instanceof Error) {
+          subscriber.error(error)
+        }
+      }
+    })
+  })
+}
