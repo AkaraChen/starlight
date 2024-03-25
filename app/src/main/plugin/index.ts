2a38f899fae77046aba7b95dbb4d5ee74526d38b
AkaraChen <akarachen@outlook.com>
2024-03-25 17:27:15 +0800

    feat: plugin manager

--- app/src/main/plugin/index.ts
+++ app/src/main/plugin/index.ts
@@ -1,8 +1,18 @@
 import { Plugin } from '@starlight-app/plugin-sdk'
 import { IocContext } from 'power-di'
+import {
+  ITranformedCommand,
+  ITransformedPlugin,
+  ITransformedView,
+  transformPlugin
+} from './transform'
 
 export class PluginManager {
   context = new IocContext()
+
+  plugins: ITransformedPlugin[] = []
+  commands: ITranformedCommand[] = []
+  views: ITransformedView[] = []
 
   callWithErrorHandling(plugin: Plugin, callback: () => void): void {
     try {
@@ -15,6 +25,17 @@ export class PluginManager {
   }
 
   resister(plugin: Plugin): void {
+    if (this.context.has(plugin.metaData.name)) {
+      throw new Error(`Plugin with name ${plugin.metaData.name} already exists`)
+    }
+    const transformed = transformPlugin(plugin)
+    if (transformed.commands) {
+      this.commands.push(...transformed.commands)
+    }
+    if (transformed.views) {
+      this.views.push(...transformed.views)
+    }
+    this.plugins.push(transformed)
     this.context.register(plugin, plugin.metaData.name, {
       singleton: true
     })
@@ -26,6 +47,9 @@ export class PluginManager {
     const instance = this.context.get(Plugin)
     this.callWithErrorHandling(plugin, () => instance.lifecycle?.deactivate?.())
     this.context.remove(plugin.metaData.name)
+    this.plugins = this.plugins.filter((p) => p.id !== plugin.metaData.name)
+    this.commands = this.commands.filter((c) => c.pluginId !== plugin.metaData.name)
+    this.views = this.views.filter((v) => v.pluginId !== plugin.metaData.name)
   }
 
   getById<T>(id: string): T {
