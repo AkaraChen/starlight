4012a51c1620b68d471dc1afa27fb81d7be8a30f
AkaraChen <akarachen@outlook.com>
2024-03-27 09:58:21 +0800

    feat: execute command

--- app/src/main/server/router/plugin.ts
+++ app/src/main/server/router/plugin.ts
@@ -5,29 +5,56 @@ import Fuse from 'fuse.js'
 import { ITranformedCommand } from '../../plugin/transform'
 import { zValidator } from '@hono/zod-validator'
 import { z } from 'zod'
+import { HTTPException } from 'hono/http-exception'
 
 const manager = PluginManager.getInstance()
 
-export const plugin = new Hono().get(
-  '/search',
-  zValidator(
-    'query',
-    z.object({
-      keyword: z.string()
-    })
-  ),
-  async (c) => {
-    const { keyword } = c.req.valid('query')
-    return streamSSE(c, async (sse) => {
-      const fuse = new Fuse(manager.commands, {
-        keys: ['description', 'displayName'] as Array<keyof ITranformedCommand>
+export const plugin = new Hono()
+  .get(
+    '/search',
+    zValidator(
+      'query',
+      z.object({
+        keyword: z.string()
       })
-      const result = fuse.search(keyword)
-      sse.writeSSE({
-        data: JSON.stringify(result)
+    ),
+    async (c) => {
+      const { keyword } = c.req.valid('query')
+      return streamSSE(c, async (sse) => {
+        const fuse = new Fuse(manager.commands, {
+          keys: ['description', 'displayName'] as Array<keyof ITranformedCommand>
+        })
+        const result = fuse.search(keyword)
+        sse.writeSSE({
+          data: JSON.stringify(result)
+        })
+        // TODO: add more search results
+        sse.close()
       })
-      // TODO: add more search results
-      sse.close()
-    })
-  }
-)
+    }
+  )
+  .post(
+    '/execute',
+    zValidator(
+      'json',
+      z.object({
+        pluginId: z.string(),
+        commandName: z.string()
+      })
+    ),
+    (c) => {
+      const { pluginId, commandName } = c.req.valid('json')
+      const command = manager.commands.find(
+        (command) => command.pluginId === pluginId && command.displayName === commandName
+      )
+      if (!command) {
+        throw new HTTPException(404, {
+          message: 'Command not found'
+        })
+      }
+      command.handler()
+      return c.json({
+        message: 'Command executed'
+      })
+    }
+  )
