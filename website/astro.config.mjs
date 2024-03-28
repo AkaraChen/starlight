c8ea751a865ef0fd9a1277b3a87a5674b0d9a7d5
AkaraChen <akarachen@outlook.com>
2024-03-28 18:51:24 +0800

    ci: try to fix

--- website/astro.config.mjs
+++ website/astro.config.mjs
@@ -8,6 +8,6 @@ import robotsTxt from "astro-robots-txt";
 export default defineConfig({
   site: import.meta.env.DEV
     ? "http://localhost:4321"
-    : "https://luna-landing-rust.vercel.app/",
+    : "https://starlight-website.vercel.app",
   integrations: [tailwind(), sitemap(), robotsTxt()],
 });
