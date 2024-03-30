9d14ac79f95468fb3f49182fad3cb1843a316078
AkaraChen <akarachen@outlook.com>
2024-03-30 17:56:15 +0800

    style: lint

--- plugins/app-launcher/tsup.config.ts
+++ plugins/app-launcher/tsup.config.ts
@@ -1,7 +1,7 @@
-import { defineConfig } from "tsup";
+import { defineConfig } from 'tsup'
 
 export default defineConfig({
   entry: ['./src/index.ts'],
   format: ['cjs'],
-  dts: true,
+  dts: true
 })
